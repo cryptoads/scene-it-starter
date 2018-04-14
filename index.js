05c4732fba255ce5e27f97cc78697e8981a4194a
Chris Michels <chrisjohnmichels@gmail.com>
2018-04-14 13:00:46 -0400

    fixed margin

--- index.js
+++ index.js
@@ -2,7 +2,7 @@ $(function(){
 var renderMovies = function(movieArray) {
     var finalHTML = '';
     movieArray.forEach( function(movie) {
-        finalHTML += '<div class = "movie card col-3 mx-1">'
+        finalHTML += '<div class = "movie card col-3 mt-1 mx-1">'
         finalHTML += '<img src="'+movie.Poster+'" class = "card-img-top">'
         finalHTML += '<div class="card-body">'
         finalHTML += '<h1>'+ movie.Title +'</h1>'
